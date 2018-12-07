631ee646e5224a734f2d9394a4bd7576b8f3caae
Thomas Verwoerd <thomas@keystone-strategic.com>
2018-12-07 12:13:32 +0100

    Reduced day6 second solution code

--- src/day6/second/Solution.kt
+++ src/day6/second/Solution.kt
@@ -20,20 +20,13 @@ fun main() = timeSolution {
     }
   }
   // all values are always connected to their neighbour coordinates(e.g. no gaps in between, so ignoring region detection since its not clear what a region is)
-  val total = distanceMap.mapIndexed { x, values ->
-    values.mapIndexed { y, distance -> Pair(Coordinate(x, y), distance) }
-  }.flatten()
-    .filter { it.second < 10000 }
-    .count()
+  val total = distanceMap.flatten().count { it < 10000 }
   println("Solution: $total")
 
 }
 
 data class Coordinate(val x: Int, val y: Int)
 
-fun toCoordinate(line: String): Coordinate {
-  val split = line.split(", ")
-  return Coordinate(x = split[0].toInt(), y = split[1].toInt())
-}
+fun toCoordinate(line: String): Coordinate = line.split(", ").let { Coordinate(x = it[0].toInt(), y = it[1].toInt()) }
 
 fun manhattanDistance(a: Coordinate, b: Coordinate) = abs(a.x - b.x) + abs(a.y - b.y)
